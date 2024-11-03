9f39584c15afe87240065517f365d6d53c4fbbc4
Jessica <jessicaalesena@gmail.com>
2024-11-03 19:32:05 -0300

    Restructuración de Proyecto: separación de FrontEnd Y BackEnd

--- src/components/ProductList.js
+++
@@ -1,7 +0,0 @@
-// src/components/ProductList.js
-export const products = [
-  { id: 1, name: "Laptop", category: "Electronics", stock: 10 },
-  { id: 2, name: "Chair", category: "Furniture", stock: 5 },
-  { id: 2, name: "Chair", category: "Furniture", stock: 5 },
- 
-];
